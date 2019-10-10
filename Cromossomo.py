6eb3d8d2c7ad56ec580703ccd20da38c556335cb
Natan Barbosa <42820875+NatanNMB15@users.noreply.github.com>
2019-10-10 20:47:43 -0300

    Teste 2.0

--- Cromossomo.py
+++ Cromossomo.py
@@ -9,17 +9,15 @@ class Cromossomo():
     def __init__(self, tamanho, limiteMaximo, limiteMinimo):
 
         self.tamanho = tamanho
-        self.valor = ""
-        self.avaliacao = -1
-        self.avaliacaoLista = []
-        self.valorX = 0
-        self.valorY = 0
+        self.valorX = ""
+        self.valorY = ""
         self.limiteMaximo = limiteMaximo
         self.limiteMinimo = limiteMinimo
 
-    def set_valor(self, novo_valor):
+    def set_valor(self, valorX, valorY):
 
-        self.valor = novo_valor
+        self.valorX = valorX
+        self.valorY = valorY
     
     def get_bin(self, x, n):
         """
@@ -39,56 +37,72 @@ class Cromossomo():
         return format(x, 'b').zfill(n)
 
     def inicializar(self):
-        novo_valor = ""
         for i in range(self.tamanho):
-            num = random.randrange(self.limiteMinimo, self.limiteMaximo, 2)
-            novo_valor = self.get_bin(num, self.tamanho)
-        self.set_valor(novo_valor)
+            for j in range(2):
+                numX = random.randrange(self.limiteMinimo, self.limiteMaximo, 2)
+                self.valorX = self.get_bin(numX, self.tamanho)
+                numY = random.randrange(self.limiteMinimo, self.limiteMaximo, 2)
+                self.valorY = self.get_bin(numY, self.tamanho)
 
     # TODO Valor minímo e máximo
     def crossover(self, outro_cromossomo, chance_crossover):
         split_index = int(random.random() * self.tamanho)
-        novo_valor = ""
+        novo_valorX = ""
+        novo_valorY = ""
         if random.random() < chance_crossover:
-            novo_valor = self.valor[0:split_index] + outro_cromossomo.valor[split_index:len(outro_cromossomo.valor)]
+            novo_valorX = self.valorX[0:split_index] + outro_cromossomo.valorX[split_index:len(outro_cromossomo.valorX)]
+            novo_valorY = self.valorY[0:split_index] + outro_cromossomo.valorY[split_index:len(outro_cromossomo.valorY)]
         else:
-            novo_valor = outro_cromossomo.valor[0:split_index] + self.valor[split_index:len(outro_cromossomo.valor)]
+            novo_valorX = outro_cromossomo.valorX[0:split_index] + self.valorX[split_index:len(outro_cromossomo.valorX)]
+            novo_valorY = outro_cromossomo.valorY[0:split_index] + self.valorY[split_index:len(outro_cromossomo.valorY)]
         novo_cromossomo = Cromossomo(self.tamanho, self.limiteMaximo, self.limiteMinimo)
-        if int(novo_valor) > self.limiteMaximo:
-            novo_valor = self.get_bin(self.limiteMaximo, self.tamanho)
-            novo_cromossomo.set_valor(novo_valor)
-        elif int(novo_valor) < self.limiteMinimo:
-            novo_valor = self.get_bin(self.limiteMinimo, self.tamanho)
-            novo_cromossomo.set_valor(novo_valor)
-        else:
-            novo_cromossomo.set_valor(novo_valor)
+        
+        if int(novo_valorX, 2) > self.limiteMaximo:
+            novo_valorX = self.get_bin(self.limiteMaximo, self.tamanho)
+            if int(novo_valorY) > self.limiteMaximo:
+                novo_valorY = self.get_bin(self.limiteMaximo, self.tamanho)
+            elif int(novo_valorY) < self.limiteMinimo:
+                novo_valorY = self.get_bin(self.limiteMinimo, self.tamanho)
+        elif int(novo_valorX) < self.limiteMinimo:
+            novo_valorX = self.get_bin(self.limiteMinimo, self.tamanho)
+            if int(novo_valorY) > self.limiteMaximo:
+                novo_valorY = self.get_bin(self.limiteMaximo, self.tamanho)
+            elif int(novo_valorY) < self.limiteMinimo:
+                novo_valorY = self.get_bin(self.limiteMinimo, self.tamanho)
+
+        novo_cromossomo.set_valor(novo_valorX, novo_valorY)
+        
         return novo_cromossomo
 
     def mutacao(self, chance_mutacao):
 
         inicio, aux, fim = ['','','']
+        novo_valorX = ""
+        novo_valorY = ""
         for i in range(self.tamanho):
             if random.random() < chance_mutacao:
-                inicio = self.valor[0:i]
-                fim = self.valor[i+1:self.tamanho]
-                aux = self.valor[i]
-                self.set_valor(inicio+aux+fim)
+                inicio = self.valorX[0:i]
+                fim = self.valorX[i+1:self.tamanho]
+                aux = self.valorX[i]
+                novo_valorX = inicio+aux+fim
+        for i in range(self.tamanho):
+            if random.random() < chance_mutacao:
+                inicio = self.valorY[0:i]
+                fim = self.valorY[i+1:self.tamanho]
+                aux = self.valorY[i]
+                novo_valorY = inicio+aux+fim
+        
+        self.set_valor(novo_valorX, novo_valorY)
 
-    def valor_real(self, inf = 0, sup = 100):
-
-        return inf + (sup - inf)/(2**self.tamanho - 1)*int(self.valor, 2)
-
-    def avaliar(self, populacao):
-
-        self.valorX = 
-        self.valorY = random.randrange(self.limiteMinimo, self.limiteMaximo)
-
-        x = self.valorX
-        y = self.valorY
+    def avaliar(self):
+        print("Valor X %d", self.valorX)
+        print("Valor Y %d", self.valorY)
+        x = int(self.valorX, 2)
+        y = int(self.valorY, 2)
 
         self.avaliacao = (1-x)**2+100*(y-x**2)**2
         return self.avaliacao
 
     def __repr__(self):
-        return "cromossomo:[%s] fitness[%.2f] valor[%d] X[%d] Y[%d]" % (self.valor, self.avaliacao, int(self.valor, 2), self.valorX, self.valorY)
+        return "cromossomo: X[%s] Y[%s] fitness[%.2f] valor X[%d] valor Y[%d]" % (self.valorX, self.valorY, self.avaliacao, int(self.valorX, 2), int(self.valorY, 2))
         
